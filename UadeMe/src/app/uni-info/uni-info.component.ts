1531611761bb3d2505a1ce000546972dc67981cc
xydownik <sazanovaa2@gmail.com>
2024-04-01 13:28:31 +0500

    initial commit

---
+++ UadeMe/src/app/uni-info/uni-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uni-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './uni-info.component.html',
+  styleUrl: './uni-info.component.css'
+})
+export class UniInfoComponent {
+
+}
