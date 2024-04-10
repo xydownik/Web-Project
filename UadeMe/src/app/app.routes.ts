55cf35af5b1a20a2c3e4a69f532c13ef04081f33
xydownik <sazanovaa2@gmail.com>
2024-04-10 22:20:09 +0500

    2nd

--- UadeMe/src/app/app.routes.ts
+++ UadeMe/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {TestPageComponent} from "./test-page/test-page.component";
+import {UniListComponent} from "./uni-list/uni-list.component";
+import {UniInfoComponent} from "./uni-info/uni-info.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent, title: 'Home'},
+  {path: 'home/test', component: TestPageComponent, title: 'Test Page'},
+  {path: 'home/universities', component: UniListComponent, title: 'Universities'},
+  {path: 'home/universities/:id', component: UniInfoComponent, title: 'University Information'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+
+];
