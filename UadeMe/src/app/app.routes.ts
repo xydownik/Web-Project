fa3a1777cf0280fd21e8a406146d6a0fe298cd77
xydownik <sazanovaa2@gmail.com>
2024-04-11 11:54:15 +0500

    TestLanding

--- UadeMe/src/app/app.routes.ts
+++ UadeMe/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {HomeComponent} from "./home/home.component";
 import {TestPageComponent} from "./test-page/test-page.component";
 import {UniListComponent} from "./uni-list/uni-list.component";
 import {UniInfoComponent} from "./uni-info/uni-info.component";
+import {SpecialsComponent} from "./specials/specials.component";
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent, title: 'Home'},
   {path: 'home/test-page', component: TestPageComponent, title: 'Test Page'},
+  {path: 'home/specialties', component: SpecialsComponent, title: 'Specialties'},
   {path: 'home/universities', component: UniListComponent, title: 'Universities'},
   {path: 'home/universities/:id', component: UniInfoComponent, title: 'University Information'},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
--- UadeMe/src/app/header/header.component.ts
+++ UadeMe/src/app/header/header.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {RouterModule} from "@angular/router";
+import {TestPageComponent} from "../test-page/test-page.component";
+import {HomeComponent} from "../home/home.component";
+import {UniversitiesComponent} from "../universities/universities.component";
+import {UniListComponent} from "../uni-list/uni-list.component";
+import {SpecialsComponent} from "../specials/specials.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,TestPageComponent, UniListComponent, SpecialsComponent ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
