3a96bb96fea087e24a968fd9d3825d9bfcf0befa
xydownik <sazanovaa2@gmail.com>
2024-04-18 13:32:03 +0500

    Consultation in process

--- UadeMe/src/app/universities.ts
+++
@@ -1,9 +0,0 @@
-export interface Universities {
-    id: number; 
-    name: string; 
-    description: string; 
-    city: string; 
-    photo: string; 
-    specialties: string; 
-    price: number; 
-}
