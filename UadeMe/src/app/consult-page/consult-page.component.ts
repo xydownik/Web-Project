c90d36f81aebb85b583dbb30798b7d0967185008
xydownik <sazanovaa2@gmail.com>
2024-04-19 22:40:48 +0500

    added consulter

--- UadeMe/src/app/consult-page/consult-page.component.ts
+++ UadeMe/src/app/consult-page/consult-page.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterModule} from "@angular/router";
+import {Consultant} from "../models";
+import {ConsultService} from "../consult.service";
+import {NgForOf, NgIf} from "@angular/common";
+import {FooterComponent} from "../footer/footer.component";
+import {ConsultItemComponent} from "../consult-item/consult-item.component";
 
 @Component({
   selector: 'app-consult-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgForOf, NgIf, FooterComponent, ConsultItemComponent],
   templateUrl: './consult-page.component.html',
   styleUrl: './consult-page.component.css'
 })
-export class ConsultPageComponent {
+export class ConsultPageComponent implements OnInit{
+  consultants!: Consultant[]
+  constructor(private consultService: ConsultService) {
+    this.consultants = []
+  }
+
+  ngOnInit(): void {
+    this.getConsultants()
+  }
+
+  getConsultants(){
+    this.consultants = this.consultService.getConsultants()
+  }
+
+
 
 }
--- UadeMe/src/app/consult.service.ts
+++ UadeMe/src/app/consult.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Consultant} from "./models";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsultService {
+  BASE_URL = "http://127.0.0.1:8000/api/consultants/"
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  static constList: Consultant[] = [
+    {
+      id: 1,
+      name: "Билялова Мадина Талгатовна",
+      profession: "профориентолог",
+      phone: "+77017443517",
+      telegram: "",
+      whatsApp: "+77017443517",
+      insta: "@Bilyalova.coach",
+      mail: "Bilyalova.coach@gmail.com ",
+      photo: '/assets/images/consultant.png',
+      rating: 4.5
+    },
+    {
+      id: 2,
+      name: "Билялова Мадина Талгатовна",
+      profession: "профориентолог",
+      phone: "+77017443517",
+      telegram: "",
+      whatsApp: "+77017443517",
+      insta: "@Bilyalova.coach",
+      mail: "Bilyalova.coach@gmail.com ",
+      photo: '/assets/images/consultant.png',
+      rating: 5
+    },
+    {
+      id :3,
+      name: "Билялова Мадина Талгатовна",
+      profession: "профориентолог",
+      phone: "+77017443517",
+      telegram: "",
+      whatsApp: "+77017443517",
+      insta :"@Bilyalova.coach",
+      mail: "Bilyalova.coach@gmail.com ",
+      photo: "/assets/images/consultant.png",
+      rating: 3.5
+    },
+    {
+      id :4,
+      name: "Билялова Мадина Талгатовна",
+      profession: "профориентолог",
+      phone: "+77017443517",
+      telegram: "",
+      whatsApp: "+77017443517",
+      insta :"@Bilyalova.coach",
+      mail: "Bilyalova.coach@gmail.com ",
+      photo: "/assets/images/consultant.png",
+      rating: 3.5
+    }
+  ]
+  getConsultants(){
+    return ConsultService.constList
+  }
+  getConsultantsObs(): Observable<Consultant[]>{
+    return this.httpClient.get<Consultant[]>(`${this.BASE_URL}`)
+  }
+  getConsultant(id: number){
+    this.httpClient.get<Consultant>(`${this.BASE_URL}${id}`)
+  }
 }
