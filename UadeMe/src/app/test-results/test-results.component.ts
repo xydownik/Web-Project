fcc5b80f97826975fb72d1887265df6795336173
Akzhan <akzhansabibolda5@gmail.com>
2024-04-23 23:16:35 +0500

    upd

--- UadeMe/src/app/test-results/test-results.component.ts
+++ UadeMe/src/app/test-results/test-results.component.ts
@@ -1,25 +1,18 @@
 import { TestRes2Component } from './../test-res-2/test-res-2.component';
 import { TestRes1Component } from './../test-res-1/test-res-1.component';
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { RadarChartComponent } from '../radar-chart/radar-chart.component';
-=======
 import {RouterModule} from "@angular/router";
->>>>>>> 2cfc221ff6cf8ef10a00700a57c74f84c9f48b47
+import { TestRes3Component } from '../test-res-3/test-res-3.component';
 
 @Component({
   selector: 'app-test-results',
   standalone: true,
   imports: [
-<<<<<<< HEAD
-    TestRes1Component, 
-    TestRes2Component,
-
-=======
     TestRes1Component,
     TestRes2Component,
+    TestRes3Component,
     RouterModule
->>>>>>> 2cfc221ff6cf8ef10a00700a57c74f84c9f48b47
   ],
   templateUrl: './test-results.component.html',
   styleUrl: './test-results.component.css'
